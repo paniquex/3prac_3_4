128188510a31712e9efeb66cfadcc1430cbf2112
paniquex <paniquexx@gmail.com>
2018-11-30 22:25:36 +0300

    added reaction to SIGINT and SIGTERM

--- main.c
+++ main.c
@@ -11,22 +11,21 @@ int
 script_writer(pid_t first_pid, pid_t second_pid, pid_t third_pid, pid_t fourth_pid);
 
 int
-script_creator( char *script_name, pid_t pid);
+script_creator(const char *script_name, pid_t pid);
 
 int main() {
-    int fd = open("hello", O_CREAT, O_RDWR, 0777);
-    fprintf(stdout, "228\n");
+    fprintf(stdout, "%d\n", getpid());
     if ((MECH_PID = fork()) == 0) {
-        signal(SIGUSR1, put_item);
+        signal(USERSIGNAL1, put_item);
         do {
             pause();
         } while (mech_flag == 1);
         return 0;
     }
     if ((SCREEN_PID = fork()) == 0) {
-        //signal(SIGUSR1, deficit_money);
-        signal(SIGUSR2, enough_money);
-        signal(SIGUSR1, add_money);
+        //signal(USERSIGNAL1, deficit_money);
+        signal(USERSIGNAL2, enough_money);
+        signal(USERSIGNAL1, add_money);
         do {
             pause();
         } while (screen_flag == 1);
@@ -34,7 +33,7 @@ int main() {
     }
     //printf("%d", SCREEN_PID);
     if ((BUTTON_PID = fork()) == 0) {
-        signal(SIGUSR1, click);
+        signal(USERSIGNAL1, click);
         do {
             pause();
         } while (button_flag == 1);
@@ -42,13 +41,15 @@ int main() {
     }
     if ((NOTESCHANGER_PID = fork()) == 0) {
         //pause();
-        signal(SIGUSR1, receive_money);
+        signal(USERSIGNAL1, receive_money);
         do {
             pause();
         } while (notechanger_flag == 1);
         return 0;
     }
     script_writer(SCREEN_PID, MECH_PID, NOTESCHANGER_PID, BUTTON_PID);
+    signal(SIGTERM, sigterm_handler);
+    signal(SIGINT, sigint_handler);
     wait(NULL);
     wait(NULL);
     wait(NULL);
@@ -59,15 +60,15 @@ int main() {
 
 int
 script_writer(pid_t first_pid, pid_t second_pid, pid_t third_pid, pid_t fourth_pid) {
-    script_creator("screen_script.sh", first_pid);
-    script_creator("mech_script.sh", second_pid);
-    script_creator("notechanger_script.sh", third_pid);
-    script_creator("button_script.sh", fourth_pid);
+    script_creator(SCREEN_SCRIPT_NAME, first_pid);
+    script_creator(MECH_SCRIPT_NAME, second_pid);
+    script_creator(NOTESCHANGER_SCRIPT_NAME, third_pid);
+    script_creator(BUTTON_SCRIPT_NAME, fourth_pid);
     return 0;
 }
 
 int
-script_creator( char *script_name, pid_t pid) {
+script_creator(const char *script_name, pid_t pid) {
     FILE *file = fopen(script_name, "w+");
     char *start_str = "#!/bin/bash\nkill ";
     fprintf(file, "%s", start_str);
