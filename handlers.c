9081d41229dc61ec20352b416597dc11661841e9
paniquex <paniquexx@gmail.com>
2018-11-30 22:24:32 +0300

    added sigterm and sigint handlers, USERSIGNALS consts

--- handlers.c
+++ handlers.c
@@ -1,8 +1,15 @@
 #include "handlers.h"
 
 int amount = 35;
-const ITEM_COST = 20;
-enum {NOTE1 = 25, NOTE2 = 50};
+const int ITEM_COST = 20;
+
+
+const char* SCREEN_SCRIPT_NAME = "screen_script.sh";
+const char* MECH_SCRIPT_NAME = "mech_script.sh";
+const char* NOTESCHANGER_SCRIPT_NAME = "noteschanger_script.sh";
+const char* BUTTON_SCRIPT_NAME = "button_script.sh";
+
+
 
 /* SCREEN handlers */
 void
@@ -23,7 +30,7 @@ enough_money(int sig) {
     char *amountStatus = "Current amount: ";
     if (amount >= ITEM_COST) {
         amount -= ITEM_COST;
-        kill(MECH_PID, SIGUSR1);
+        kill(MECH_PID, USERSIGNAL1);
         fprintf(stdout, "%s", amountStatus);
         fprintf(stdout, "%d\n", amount);
     } else {
@@ -64,15 +71,42 @@ put_item(int sig) {
 /* NOTECHANGER handlers */
 void
 receive_money(int sig) {
-    kill(SCREEN_PID, SIGUSR1);
+    kill(SCREEN_PID, USERSIGNAL1);
 }
 
 
 /* BUTTON handlers */
 void
 click(int sig) {
-    kill(SCREEN_PID, SIGUSR2);
+    kill(SCREEN_PID, USERSIGNAL2);
 }
 
 
 /* SIGTERM */
+void
+sigterm_handler(int sig) {
+    kill(SCREEN_PID, SIGTERM);
+    kill(MECH_PID, SIGTERM);
+    kill(NOTESCHANGER_PID, SIGTERM);
+    kill(BUTTON_PID, SIGTERM);
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+    unlink(SCREEN_SCRIPT_NAME);
+    unlink(MECH_SCRIPT_NAME);
+    unlink(NOTESCHANGER_SCRIPT_NAME);
+    unlink(BUTTON_SCRIPT_NAME);
+}
+
+
+/*SIGINT */
+void
+sigint_handler(int sig) {
+    printf("*****  STATUS  *****\n");
+    kill(SCREEN_PID, SIGINT);
+    kill(MECH_PID, SIGINT);
+    kill(NOTESCHANGER_PID, SIGINT);
+    kill(BUTTON_PID, SIGINT);
+    printf("*****  STATUS  *****\n");
+}
--- handlers.h
+++ handlers.h
@@ -5,6 +5,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <wait.h>
+
+const char* SCREEN_SCRIPT_NAME;
+const char* MECH_SCRIPT_NAME;
+const char* NOTESCHANGER_SCRIPT_NAME;
+const char* BUTTON_SCRIPT_NAME;
+
+enum {NOTE1 = 25, NOTE2 = 50};
+enum {USERSIGNAL1 = 30, USERSIGNAL2 = 10, USERSIGNAL3 = 16,
+    USERSIGNAL4 = 31, USERSIGNAL5 = 12, USERSIGNAL6 = 17};
 
 pid_t SCREEN_PID, BUTTON_PID, NOTESCHANGER_PID, MECH_PID; // const but no? codestyle!!
 
@@ -33,6 +43,15 @@ receive_money(int sig);
 void
 click(int sig);
 
+
+/*SIGTERM */
+void
+sigterm_handler(int sig);
+
+
+/*SIGINT */
+void
+sigint_handler(int sig);
 
 
 #endif //PRAC3_3_4_HANDLERS_H
