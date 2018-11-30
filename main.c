70cfc587e93ec6bc6182769f8cfabb55a98177ee
paniquex <paniquexx@gmail.com>
2018-11-30 09:25:15 +0300

    add creating processes and set different reactions on signals. Need to rewrite SIGTERM/SIGINT handler

--- main.c
+++ main.c
@@ -2,33 +2,76 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <wait.h>
+#include <string.h>
+#include "handlers.h"
 
-pid_t SCREEN_PID, BUTTON_PID, NOTESCHANGER_PID, MECH_PID; //consts, but no??
+int screen_flag = 1, mech_flag = 1, notechanger_flag = 1, button_flag = 1;
+
+int
+script_writer(pid_t first_pid, pid_t second_pid, pid_t third_pid, pid_t fourth_pid);
+
+int
+script_creator( char *script_name, pid_t pid);
 
 int main() {
     int fd = open("hello", O_CREAT, O_RDWR, 0777);
-    if ((SCREEN_PID = fork()) == 0) {
-        //pause();
+    fprintf(stdout, "228\n");
+    if ((MECH_PID = fork()) == 0) {
+        signal(SIGUSR1, put_item);
+        do {
+            pause();
+        } while (mech_flag == 1);
         return 0;
     }
-    write(1, &SCREEN_PID, 1);
+    if ((SCREEN_PID = fork()) == 0) {
+        //signal(SIGUSR1, deficit_money);
+        signal(SIGUSR2, enough_money);
+        signal(SIGUSR1, add_money);
+        do {
+            pause();
+        } while (screen_flag == 1);
+        return 0;
+    }
+    //printf("%d", SCREEN_PID);
     if ((BUTTON_PID = fork()) == 0) {
-        SCREEN_PID = 23;
-        write(1, &SCREEN_PID, sizeof(int));
-        //pause();
+        signal(SIGUSR1, click);
+        do {
+            pause();
+        } while (button_flag == 1);
         return 0;
     }
     if ((NOTESCHANGER_PID = fork()) == 0) {
         //pause();
+        signal(SIGUSR1, receive_money);
+        do {
+            pause();
+        } while (notechanger_flag == 1);
         return 0;
     }
-    if ((MECH_PID = fork()) == 0) {
-        //pause();
-        return 0;
-    }
+    script_writer(SCREEN_PID, MECH_PID, NOTESCHANGER_PID, BUTTON_PID);
     wait(NULL);
     wait(NULL);
     wait(NULL);
     wait(NULL);
+    return 0;
+}
+
+
+int
+script_writer(pid_t first_pid, pid_t second_pid, pid_t third_pid, pid_t fourth_pid) {
+    script_creator("screen_script.sh", first_pid);
+    script_creator("mech_script.sh", second_pid);
+    script_creator("notechanger_script.sh", third_pid);
+    script_creator("button_script.sh", fourth_pid);
+    return 0;
+}
+
+int
+script_creator( char *script_name, pid_t pid) {
+    FILE *file = fopen(script_name, "w+");
+    char *start_str = "#!/bin/bash\nkill ";
+    fprintf(file, "%s", start_str);
+    fprintf(file, "%d\n", pid);
+    fclose(file);
     return 0;
 }
