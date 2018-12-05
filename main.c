abf5456b4efe3a66b7c0fe0b5631ee52d69cc125
paniquex <paniquexx@gmail.com>
2018-12-05 20:04:09 +0300

    Code was refactored: made handlers very short, all work is doing in sons

--- main.c
+++ main.c
@@ -3,7 +3,78 @@
 #include <unistd.h>
 #include <wait.h>
 #include <string.h>
-#include "handlers.h"
+//#include "handlers.h"
+
+
+int amount = 30;
+const int ITEM_COST = 20;
+
+const char* SCREEN_SCRIPT_NAME = "screen_script.sh";
+const char* MECH_SCRIPT_NAME = "mech_script.sh";
+const char* NOTESCHANGER_SCRIPT_NAME = "noteschanger_script.sh";
+const char* BUTTON_SCRIPT_NAME = "button_script.sh";
+
+enum {NOTE1 = 25, NOTE2 = 60};
+enum {USERSIGNAL1 = 30, USERSIGNAL2 = 10, USERSIGNAL3 = 16,
+    USERSIGNAL4 = 31, USERSIGNAL5 = 12, USERSIGNAL6 = 17};
+
+int screen_status, mech_status, noteschanger_status, button_status;
+
+int button_ready_to_click, button_clicks;
+
+pid_t SCREEN_PID, BUTTON_PID, NOTESCHANGER_PID, MECH_PID;
+
+
+/* SCREEN handlers */
+void
+enough_money(int sig);
+
+void
+add_money(int sig);
+
+void
+status_screen(int sig);
+
+void
+alarm_screen(int sig);
+
+
+/* MECH handlers: */
+void
+put_item(int sig);
+
+void
+status_mech(int sig);
+
+/* NOTECHANGER handlers: */
+void
+receive_money(int sig);
+
+
+void
+status_noteschanger(int sig);
+
+/* BUTTON handlers: */
+void
+click(int sig);
+
+void
+item_was_put_away(int sig);
+
+void
+status_button(int sig);
+
+/*SIGTERM */
+void
+sigterm_handler(int sig);
+
+
+/*SIGINT */
+void
+sigint_handler(int sig);
+
+
+
 
 int screen_flag = 1, mech_flag = 1, notechanger_flag = 1, button_flag = 1; // we can do it only one
 
@@ -29,23 +100,66 @@ int main() {
         signal(USERSIGNAL3, status_mech);
         do {
             pause();
+            if (mech_status == 2) {
+                fprintf(stdout, "Putting item away...\n");
+                unsigned int time = 3;
+                mech_status = 1;
+                sleep(time);
+                mech_status = 0;
+                fprintf(stdout, "Get your item!\n");
+                kill(BUTTON_PID, USERSIGNAL2);
+            }
+            mech_status = 0;
         } while (mech_flag == 1);
         return 0;
     }
 
 
     if ((SCREEN_PID = fork()) == 0) {
+        screen_status = 0;
         close(fd[1]);
         read(fd[0], &BUTTON_PID, sizeof(pid_t));
         close(fd[0]);
-        //signal(USERSIGNAL1, deficit_money);
+        signal(SIGALRM, alarm_screen);
+        //alarm(5);
         signal(USERSIGNAL2, enough_money);
         signal(USERSIGNAL1, add_money);
         signal(USERSIGNAL4, add_money);
         signal(SIGINT, SIG_IGN); //ignore SIGINT
         signal(USERSIGNAL3, status_screen);
+        char *amountStatus = "Current amount: ";
         do {
             pause();
+            if (screen_status == 2) { //enough_money
+                //signal(USERSIGNAL2, SIG_IGN); ignore USERSIGNAL2 from button before item has put away
+                if (amount >= ITEM_COST) {
+                    amount -= ITEM_COST;
+                    kill(MECH_PID, USERSIGNAL1);
+                    fprintf(stdout, "%s", amountStatus);
+                    fprintf(stdout, "%d\n", amount);
+                } else {
+                    unsigned int time = 3;
+                    char *notEnoughMoneyStr = "Not enough money\n";
+                    fprintf(stdout, "%s", notEnoughMoneyStr);
+                    fprintf(stdout, "%s", amountStatus);
+                    fprintf(stdout, "%d\n", amount);
+                    kill(BUTTON_PID, USERSIGNAL2);
+                    screen_status = 1;
+                    sleep(time);
+                    screen_status = 0;
+                }
+            }
+            if (screen_status == 3) { //add_money
+                amount += NOTE1;
+                fprintf(stdout, "%s", amountStatus);
+                fprintf(stdout, "%d\n", amount);
+            }
+            if (screen_status == 4) {
+                amount += NOTE2;
+                fprintf(stdout, "%s", amountStatus);
+                fprintf(stdout, "%d\n", amount);
+            }
+            screen_status = 0;
         } while (screen_flag == 1);
         return 0;
     }
@@ -62,6 +176,15 @@ int main() {
         signal(USERSIGNAL3, status_button);
         do {
             pause();
+            if (button_status == 1) {
+                if (button_ready_to_click == 1) {
+                    kill(SCREEN_PID, USERSIGNAL2);
+                    button_ready_to_click = 0;
+                    pause(); //waiting for signal from MECH, that it has done work
+                }
+                button_ready_to_click = 1;
+            }
+            button_status = 0;
         } while (button_flag == 1);
         return 0;
     }
@@ -80,6 +203,25 @@ int main() {
         signal(USERSIGNAL3, status_noteschanger);
         do {
             pause();
+            if (noteschanger_status == 2) {
+                printf("Receiving %d coins\n", NOTE1);
+                noteschanger_status = 1;
+                signal(USERSIGNAL2, SIG_IGN);
+                sleep(3);
+                signal(USERSIGNAL2, receive_money);
+                noteschanger_status = 0;
+                kill(SCREEN_PID, USERSIGNAL1);
+            }
+            if (noteschanger_status == 3) {
+                printf("Receiving %d coins\n", NOTE2);
+                noteschanger_status = 1;
+                signal(USERSIGNAL1, SIG_IGN);
+                sleep(3);
+                signal(USERSIGNAL1, receive_money);
+                noteschanger_status = 0;
+                kill(SCREEN_PID, USERSIGNAL4);
+            }
+            noteschanger_status = 0;
         } while (notechanger_flag == 1);
         return 0;
     }
@@ -113,4 +255,127 @@ script_creator(const char *script_name, pid_t pid, int signal_id) {
     fprintf(file, "%d\n", pid);
     fclose(file);
     return 0;
+}
+
+
+/* SCREEN handlers */
+void
+enough_money(int sig) { //need to rename this function
+    screen_status = 2;
+}
+
+
+void
+add_money(int sig) {
+    if (sig == USERSIGNAL1) {
+        screen_status = 3;
+    } else if (sig == USERSIGNAL4) {
+        screen_status = 4;
+    }
+}
+
+
+void
+status_screen(int sig) {
+    fprintf(stderr, "SCREEN: Current amount: %d\n", amount);
+}
+
+
+void
+alarm_screen(int sig) {
+    printf("Current amount: %d\n", amount);
+    alarm(5);
+}
+
+/* MECH handlers */
+void
+put_item(int sig) {
+    mech_status = 2;
+}
+
+
+void
+status_mech(int sig) {
+    if (mech_status == 1) {
+        fprintf(stderr, "MECH: Putting item away...\n");
+    } else {
+        fprintf(stderr, "MECH: Idle...\n");
+    }
+}
+
+
+/* NOTECHANGER handlers */
+void
+receive_money(int sig) {
+    if (noteschanger_status == 0) {
+        if (sig == USERSIGNAL1) {
+              noteschanger_status = 2;
+        } else if (sig == USERSIGNAL2) {
+            noteschanger_status = 3;
+        }
+    }
+}
+
+
+void
+status_noteschanger(int sig) {
+    if (noteschanger_status == 1) {
+        fprintf(stderr, "NOTECHANGER: Receiving money...\n");
+    } else {
+        fprintf(stderr, "NOTECHANGER: Idle...\n");
+    }
+}
+
+
+/* BUTTON handlers */
+void
+click(int sig) {
+    button_clicks++;
+    button_status = 1;
+}
+
+void
+item_was_put_away(int sig) {
+    //just for remove pause from BUTTON
+}
+
+void
+status_button(int sig) {
+    fprintf(stderr, "BUTTON: was clicked %d times.\n", button_clicks);
+}
+
+
+/* SIGTERM */
+void
+sigterm_handler(int sig) {
+    kill(SCREEN_PID, SIGTERM);
+    kill(MECH_PID, SIGTERM);
+    kill(NOTESCHANGER_PID, SIGTERM);
+    kill(BUTTON_PID, SIGTERM);
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+    unlink(SCREEN_SCRIPT_NAME);
+    unlink(MECH_SCRIPT_NAME);
+    unlink(NOTESCHANGER_SCRIPT_NAME);
+    unlink(BUTTON_SCRIPT_NAME);
+}
+
+
+/*SIGINT */
+void
+sigint_handler(int sig) {
+    printf("***************  STATUS  ***************\n");
+    sleep(1);
+    kill(SCREEN_PID, USERSIGNAL3);
+    sleep(1);
+    kill(MECH_PID, USERSIGNAL3);
+    sleep(1);
+    kill(NOTESCHANGER_PID, USERSIGNAL3);
+    sleep(1);
+    kill(BUTTON_PID, USERSIGNAL3);
+    sleep(1);
+    printf("***************  STATUS  ***************\n");
+    //pause();
 }
